f4585dc93d2ce9b5323aec38bda835ace862a2e0
CyberForker <tsempu@outlook.com>
2020-12-11 20:36:05 +0800

    LoopssWrapper

---
+++ LOOPSS/Contract/SafeMath.sol
@@ -0,0 +1,21 @@
+contract SafeMath {
+    function safeAdd(uint256 a, uint256 b) public pure returns (uint256 c) {
+        c = a + b;
+        require(c >= a);
+    }
+
+    function safeSub(uint256 a, uint256 b) public pure returns (uint256 c) {
+        require(b <= a);
+        c = a - b;
+    }
+
+    function safeMul(uint256 a, uint256 b) public pure returns (uint256 c) {
+        c = a * b;
+        require(a == 0 || c / a == b);
+    }
+
+    function safeDiv(uint256 a, uint256 b) public pure returns (uint256 c) {
+        require(b > 0);
+        c = a / b;
+    }
+}
